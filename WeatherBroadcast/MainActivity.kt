508db8c399945b6609e25f801d28f197c6291804
Ilya <altynnikovila@gmail.com>
2026-04-25 14:37:38 +0800

    Add weather caching

--- WeatherBroadcast/MainActivity.kt
+++ WeatherBroadcast/MainActivity.kt
@@ -11,19 +11,54 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
 import android.widget.Toolbar
+import android.os.Environment
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 import androidx.fragment.app.Fragment
 import org.json.JSONObject
+import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
 import java.util.Locale
 
-
 class MainActivity : AppCompatActivity() {
+    private val handler = Handler(Looper.getMainLooper())
+    private val checkInterval = 10000L
+    private val fileCheckRunnable = object : Runnable {
+        override fun run() {
+            checkFile()
+            handler.postDelayed(this, checkInterval)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        handler.post(fileCheckRunnable)
+    }
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(fileCheckRunnable)
+    }
+    private fun checkFile() {
+        val file = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+            "WeatherFile.json")
+        if (file.exists()) {
+            val isExpired = System.currentTimeMillis() - file.lastModified() > 15000
+            if (isExpired) {
+                val json = JSONObject(file.readText())
+                val name = json.optString("name")
+                runOnUiThread {
+                    Toast.makeText(this, "Updating expired weather", Toast.LENGTH_SHORT).show()
+                }
+                fetchWeather(name)
+            }
+        }
+    }
+
     val sharedViewModel: SharedViewModel by viewModels()
     private var lastCity: String? = null
     private var lastLocale: String? = null
@@ -110,37 +145,27 @@ class MainActivity : AppCompatActivity() {
             val ft = supportFragmentManager.beginTransaction()
             ft.replace(R.id.container_fragm, detailedFragment, "DETAILED_TAG")
             ft.commit() }
+
+        val file: File = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+            "WeatherFile.json")
+        if (file.exists()) {
+            Toast.makeText(this, "Reading weather from file", Toast.LENGTH_SHORT).show()
+            updateWeather()
+        }
     }
 
-    private fun fetchWeather(cityName: String) {
+    private fun updateWeather() {
         Thread {
             try {
                 val rbF =  findViewById<RadioButton>(R.id.rbF)
-                val apiKey = getString(R.string.openweather_api_key).trim()
-                val units = if (rbF.isChecked) "imperial" else "metric"
-                val q = URLEncoder.encode(cityName, "UTF-8")
-                val urlStr = "https://api.openweathermap.org/data/2.5/weather?q=$q&appid=$apiKey&units=$units&lang=$lastLocale"
-                val url = URL(urlStr)
-                val conn = url.openConnection() as HttpURLConnection
-                conn.requestMethod = "GET"
-                conn.connectTimeout = 10_000
-                conn.readTimeout = 10_000
 
-                val code = conn.responseCode
-                val stream = if (code == 200) conn.inputStream else conn.errorStream
-                val body = stream.bufferedReader().use { it.readText() }
-
-                if (code != 200) {
-                    Log.e("WeatherFetch", "code=$code body=$body")
-                    runOnUiThread {
-                        Toast.makeText(this, "Ошибка запроса: $code", Toast.LENGTH_LONG).show()
-                    }
-                    return@Thread
+                val file = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+                    "WeatherFile.json")
+                runOnUiThread {
+                    Toast.makeText(this, "Updating UI", Toast.LENGTH_SHORT).show()
                 }
-
-                val json = JSONObject(body)
-
-                val name = json.optString("name", cityName)
+                val json = JSONObject(file.readText())
+                val name = json.optString("name")
                 val main = json.optJSONObject("main")
                 val temp = main?.optDouble("temp", Double.NaN) ?: Double.NaN
                 val humidity = main?.optInt("humidity", -1) ?: -1
@@ -167,7 +192,49 @@ class MainActivity : AppCompatActivity() {
                     sharedViewModel.weatherData.iconCode.set(icon)
                     updateWindDisplay()
                 }
+            } catch (t: Throwable) {
+                t.printStackTrace()
+                runOnUiThread {
+                    Toast.makeText(this,
+                        getString(R.string.error, t.localizedMessage), Toast.LENGTH_LONG).show()
+                }
+            }
+        }.start()
+    }
+    private fun fetchWeather(cityName: String) {
+        Thread {
+            try {
+                val rbF =  findViewById<RadioButton>(R.id.rbF)
+                val apiKey = getString(R.string.openweather_api_key).trim()
+                val units = if (rbF.isChecked) "imperial" else "metric"
+                val q = URLEncoder.encode(cityName, "UTF-8")
+                val urlStr = "https://api.openweathermap.org/data/2.5/weather?q=$q&appid=$apiKey&units=$units&lang=$lastLocale"
+                val url = URL(urlStr)
+                val conn = url.openConnection() as HttpURLConnection
+                conn.requestMethod = "GET"
+                conn.connectTimeout = 10_000
+                conn.readTimeout = 10_000
 
+                val code = conn.responseCode
+                val stream = if (code == 200) conn.inputStream else conn.errorStream
+                val body = stream.bufferedReader().use { it.readText() }
+
+                if (code != 200) {
+                    Log.e("WeatherFetch", "code=$code body=$body")
+                    runOnUiThread {
+                        Toast.makeText(this, "Ошибка запроса: $code", Toast.LENGTH_LONG).show()
+                    }
+                    return@Thread
+                }
+
+                val file = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+                    "WeatherFile.json")
+                val json = JSONObject(body)
+                runOnUiThread {
+                    Toast.makeText(this, "Writing weather to a file", Toast.LENGTH_SHORT).show()
+                }
+                file.writeText(json.toString(4))
+                updateWeather()
             } catch (t: Throwable) {
                 t.printStackTrace()
                 runOnUiThread {
